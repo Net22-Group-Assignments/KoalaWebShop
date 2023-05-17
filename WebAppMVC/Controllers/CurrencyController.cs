ca765f38c7121d575e3d2fc606b0f1df017c0d29
jonwestman <jonwestman88@gmail.com>
2023-05-17 13:28:52 +0200

    commented out api stuff

--- WebAppMVC/Controllers/CurrencyController.cs
+++ WebAppMVC/Controllers/CurrencyController.cs
@@ -1,43 +1,42 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Azure.Core;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.DotNet.Scaffolding.Shared.CodeModifier.CodeChange;
 using Newtonsoft.Json;
+using System;
 using WebAppMVC.Models.ViewModels;
 
 namespace WebAppMVC.Controllers
 {
     public class CurrencyController : Controller
     {
-        //        var client = new RestClient("https://api.apilayer.com/exchangerates_data/convert?to={to}&from={from}&amount={amount}");
-        //        client.Timeout = -1;
+        //public async Task<IActionResult> GetCurrency()
+        //{
+        //    var client = new RestClient("https://api.apilayer.com/exchangerates_data/latest?symbols={symbols}&base={base}");
+        //    client.Timeout = -1;
 
-        //var request = new RestRequest(Method.GET);
-        //        request.AddHeader("apikey", "IteZQQFBVQ7bcr481MmJ04hfqwSctgFo");
+        //    var request = new Restrequest(Method.GET);
+        //    request.AddHeader("apikey", "IteZQQFBVQ7bcr481MmJ04hfqwSctgFo");
 
-        //IRestResponse response = client.Execute(request);
-        //        Console.WriteLine(response.Content);
+        //    IRestResponse response = client.Execute(request);
+        //    Console.WriteLine(response.Content);
 
-        Uri baseAdress = new Uri("https://api.apilayer.com/exchangerates_data/convert?to={to}&from={from}&amount={amount}");
-        private readonly HttpClient _client;
+        //    return View(response);
+        //}
 
-        public CurrencyController()
-        {
-            _client = new HttpClient();
-            _client.BaseAddress = baseAdress;
-        }
-
-        [HttpGet]
-        public IActionResult Index()
-        {
-            List<CurrencyViewModel> currencyList = new List<CurrencyViewModel>();
-            HttpResponseMessage response = _client.GetAsync(_client.BaseAddress + "/currency/Get").Result;
-
-            if (response.IsSuccessStatusCode)
-            {
-                string data = response.Content.ReadAsStringAsync().Result;
-                currencyList = JsonConvert.DeserializeObject<List<CurrencyViewModel>>(data);
-            }
-
-            return View(currencyList);
-        }
+        //public async Task<IActionResult> Index()
+        //{
+        //    List<CurrencyViewModel> currencyList = new List<CurrencyViewModel>();
+        //    using (var httpClient = new HttpClient())
+        //    {
+        //        var response = await httpClient.GetAsync("https://api.apilayer.com/exchangerates_data/latest?symbols=SEK,USD,EUR&base=SEK");
+        //        var request = response.Headers.Add("apikey", "IteZQQFBVQ7bcr481MmJ04hfqwSctgFo");
+        //        using (request)
+        //        {
+        //            string apiResponse = await request.Content.ReadAsStringAsync();
+        //            currencyList = JsonConvert.DeserializeObject<List<CurrencyViewModel>>(apiResponse);
+        //        }
+        //    }
+        //        return View(currencyList);
+        //}
     }
 }
--- WebAppMVC/Models/ViewModels/CurrencyViewModel.cs
+++ WebAppMVC/Models/ViewModels/CurrencyViewModel.cs
@@ -3,22 +3,9 @@
     public class CurrencyViewModel
     {
         public DateTime Date { get; set; }
-        public int MyProperty { get; set; }
-
-          //      {
-          //"date": "2018-02-22",
-          //"historical": "",
-          //"info": {
-          //  "rate": 148.972231,
-          //  "timestamp": 1519328414
-          //},
-          //"query": {
-          //  "amount": 25,
-          //  "from": "GBP",
-          //  "to": "JPY"
-          //},
-          //"result": 3724.305775,
-          //"success": true
-          //      }
+        public string Base { get; set; }
+        public decimal SEKrate { get; set; }
+        public decimal USDrate { get; set; }
+        public decimal EURrate { get; set; }
     }
 }
