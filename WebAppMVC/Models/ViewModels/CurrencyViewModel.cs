67902d83706c4027ef12f5e36f8c3d160cc33391
jonwestman <jonwestman88@gmail.com>
2023-05-17 10:02:56 +0200

    commented out string

--- WebAppMVC/Models/ViewModels/CurrencyViewModel.cs
+++ WebAppMVC/Models/ViewModels/CurrencyViewModel.cs
@@ -5,20 +5,20 @@
         public DateTime Date { get; set; }
         public int MyProperty { get; set; }
 
-                {
-          "date": "2018-02-22",
-          "historical": "",
-          "info": {
-            "rate": 148.972231,
-            "timestamp": 1519328414
-          },
-          "query": {
-            "amount": 25,
-            "from": "GBP",
-            "to": "JPY"
-          },
-          "result": 3724.305775,
-          "success": true
-                }
+          //      {
+          //"date": "2018-02-22",
+          //"historical": "",
+          //"info": {
+          //  "rate": 148.972231,
+          //  "timestamp": 1519328414
+          //},
+          //"query": {
+          //  "amount": 25,
+          //  "from": "GBP",
+          //  "to": "JPY"
+          //},
+          //"result": 3724.305775,
+          //"success": true
+          //      }
     }
 }
