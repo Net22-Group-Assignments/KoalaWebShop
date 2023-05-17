921c5f023a2f11575fdd483f392d2500a67c653b
jonwestman <jonwestman88@gmail.com>
2023-05-17 10:01:17 +0200

    the last commit of commitment

---
+++ WebAppMVC/Controllers/CurrencyController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.DotNet.Scaffolding.Shared.CodeModifier.CodeChange;
+using Newtonsoft.Json;
+using WebAppMVC.Models.ViewModels;
+
+namespace WebAppMVC.Controllers
+{
+    public class CurrencyController : Controller
+    {
+        //        var client = new RestClient("https://api.apilayer.com/exchangerates_data/convert?to={to}&from={from}&amount={amount}");
+        //        client.Timeout = -1;
+
+        //var request = new RestRequest(Method.GET);
+        //        request.AddHeader("apikey", "IteZQQFBVQ7bcr481MmJ04hfqwSctgFo");
+
+        //IRestResponse response = client.Execute(request);
+        //        Console.WriteLine(response.Content);
+
+        Uri baseAdress = new Uri("https://api.apilayer.com/exchangerates_data/convert?to={to}&from={from}&amount={amount}");
+        private readonly HttpClient _client;
+
+        public CurrencyController()
+        {
+            _client = new HttpClient();
+            _client.BaseAddress = baseAdress;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<CurrencyViewModel> currencyList = new List<CurrencyViewModel>();
+            HttpResponseMessage response = _client.GetAsync(_client.BaseAddress + "/currency/Get").Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                currencyList = JsonConvert.DeserializeObject<List<CurrencyViewModel>>(data);
+            }
+
+            return View(currencyList);
+        }
+    }
+}
---
+++ WebAppMVC/Models/ViewModels/CurrencyViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace WebAppMVC.Models.ViewModels
+{
+    public class CurrencyViewModel
+    {
+        public DateTime Date { get; set; }
+        public int MyProperty { get; set; }
+
+                {
+          "date": "2018-02-22",
+          "historical": "",
+          "info": {
+            "rate": 148.972231,
+            "timestamp": 1519328414
+          },
+          "query": {
+            "amount": 25,
+            "from": "GBP",
+            "to": "JPY"
+          },
+          "result": 3724.305775,
+          "success": true
+                }
+    }
+}
