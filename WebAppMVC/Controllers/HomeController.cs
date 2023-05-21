b8e4e07c929d444f62cc0ff3ef5264a15823127d
oskar åhling <112972443+EduOskar@users.noreply.github.com>
2023-05-21 23:28:23 +0200

    Added so you could edit products, added so u could get details about item

--- WebAppMVC/Controllers/HomeController.cs
+++ WebAppMVC/Controllers/HomeController.cs
@@ -17,18 +17,72 @@ namespace WebAppMVC.Controllers
             _logger = logger;
         }
 
-        public async  Task<IActionResult> Index(string sortOrder, string searchString)
+        public async Task<IActionResult> Index(string sortOrder, string searchString)
         {
             ViewData["NameSortParm"] = string.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
             ViewData["CurrentFilter"] = searchString;
-            var products = from p in _context.Products.Include(c=>c.Category)
+            var products = from p in _context.Products.Include(c => c.Category)
                            select p;
             if (!string.IsNullOrEmpty(searchString))
             {
                 products = products.Where(p => p.Title.Contains(searchString)
                                         || p.Category.Title.Contains(searchString));
             }
-            return View(await products.AsNoTracking().ToListAsync());                
+            return View(await products.AsNoTracking().ToListAsync());
+        }
+        //Get: Product
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Products == null)
+            {
+                return NotFound();
+            }
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+        //post: productedit
+        [HttpPost, ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditProductPost(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var productToUpdate = await _context.Products.AsNoTracking().FirstOrDefaultAsync(p => p.ProductId == id);
+            if (await TryUpdateModelAsync<Product>(productToUpdate, "", p => p.Title, p => p.Category, p => p.Price, p => p.Quantity))
+            {
+                try
+                {
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (DbUpdateException)
+                {
+                    ModelState.AddModelError("", "Unable tp save changes");
+                }
+            }
+            return View(productToUpdate);
+
+
+        }
+        //Get productDetails
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null ||_context.Products == null)
+            {
+                return NotFound();
+            }
+            var product = await _context.Products.FirstOrDefaultAsync(p => p.ProductId == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
         }
 
 
