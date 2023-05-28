7418f3206929914a78841f6ab1193fad0a951710
oskar åhling <112972443+EduOskar@users.noreply.github.com>
2023-05-28 20:27:18 +0200

    insignificant cvhanges

--- WebAppMVC/Models/OrderItem.cs
+++ WebAppMVC/Models/OrderItem.cs
@@ -8,7 +8,6 @@ public class OrderItem
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
-
     public int OrderId { get; set; }
     public Order Order { get; set; }
 
